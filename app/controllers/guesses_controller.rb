b10c8c26c6cd63df8b5acbe5b7dfe3cad89e100c
goatcheese82 <nonsquishy@gmail.com>
2019-01-14 22:24:13 -0600

    added params

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -2,7 +2,28 @@ class GuessesController < ApplicationController
     def index
     end
 
-    def show
+    def create
+        @guess = Guess.create(guess_params)
+        redirect_to guess_path(@guess)
     end
 
+    def new
+        @guess = Guess.new
+    end
+    
+
+    def show
+    end
+    
+
+    private
+
+    def guess_params
+        params.require(:guess).permit(
+            :name,
+            :answer
+        )
+    end
+    
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'guesses#index'
+  root 'guesses#new'
   resources :guesses
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
